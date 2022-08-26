7bd204538df80493b0f3b8d2786fc7d10dc971af
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2022-08-26 20:46:50 +0200

    Add files via upload

--- Skinny-Desktop-Setup.sh
+++ Skinny-Desktop-Setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mv /etc/apt/sources.list /etc/apt/sources.list.disable
+rm /etc/apt/sources.list.d/stable.list
+rm /etc/apt/sources.list.d/testing.list
+
 echo "deb http://deb.debian.org/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list.d/stable.list
 echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list.d/stable.list
 echo "deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list.d/stable.list
@@ -20,10 +23,15 @@ mv ./Suckless-Software/st /usr/local/bin/st
 mv ./Suckless-Software/dmenu /usr/local/bin/dmenu
 mv ./Suckless-Software/dmenu_run /usr/local/bin/dmenu_run
 mv ./Suckless-Software/dmenu_path /usr/local/bin/dmenu_path
+
+if [ $USER != "root" ] ; then
 echo -e "exec dwm" >> /home/${USER}/.xinitrc
+echo -e "TERM=xterm" >> /home/${USER}/.bashrc
 echo -e "if [ -d "usr/local/sbin" ] ; then\n   PATH=\"usr/local/sbin:$PATH\"\nfi\"" >> /home/${USER}/.profile
 echo -e "if [ -d \"/sbin\" ] ; then\n    PATH=\"/sbin:$PATH\"\nfi\"" >> /home/${USER}/.profile
 echo -e "if [[ \"$(tty)\" == \"/dev/tty1\" ]]\nthen\n        startx\nfi\"" >> /home/${USER}/.profile
+fi
+
 echo -e "export \"PATH=$PATH:/usr/local/sbin\"" >> /root/.profile
 echo -e "export \"PATH=$PATH:/usr/local/sbin\"" >> /root/.profile
 
