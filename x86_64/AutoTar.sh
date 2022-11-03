cdb2ddc023c083a6de6ed9c56ec708c95e43a169
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2022-11-03 09:40:36 +0000

    Add files via upload

---
+++ x86_64/AutoTar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "/home/hendrik/Skinny-Linux/Source"
+tar czvf ../x86_64/Skinny-Linux-appmenu.tar.gz       ./Skinny-Linux-appmenu
+tar czvf ../x86_64/Skinny-Linux-theme.tar.gz         ./Skinny-Linux-theme
+tar czvf ../x86_64/Suckless-Software.tar.gz          ./Suckless-Software
+tar czvf ../x86_64/Skinny-Neovim.tar.gz              ./Skinny-Neovim
+tar czvf ../x86_64/Skinny-Linux-fonts.tar.gz         ./Skinny-Linux-fonts
