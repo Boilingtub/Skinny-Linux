784b1221c45e4c8911b84ecfe268efae9fc107b5
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2022-11-03 07:58:43 +0000

    Delete x86-64 directory

--- x86-64/AutoTar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "/home/hendrik/Skinny-Linux"
-tar czvf x86-64/Skinny-Linux-appmenu.tar.gz       Source/Skinny-Linux-appmenu
-tar czvf x86-64/Skinny-Linux-theme.tar.gz         Source/Skinny-Linux-theme
-tar czvf x86-64/Suckless-Software.tar.gz          Source/Suckless-Software
-tar czvf x86-64/Skinny-Neovim.tar.gz              Source/Skinny-Neovim
-tar czvf x86-64/Skinny-Linux-fonts.tar.gz         Source/Skinny-Linux-fonts
