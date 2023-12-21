47cecc154389c4efe55c96abce2346e59deb70f7
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2023-12-21 21:31:08 +0200

    Add files via upload

---
+++ x86_64/AutoTar.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cd "/home/hendrik/Skinny-Linux"
+tar czvf ./Compressed-Archives/HackFont.tar.gz ./HackFont
+tar czvf ./Compressed-Archives/dwl-v0.4.tar.gz ./dwl-v0.4
+tar czvf ./Compressed-Archives/foot.tar.gz ./foot
+tar czvf ./Compressed-Archives/kickoff.tar.gz ./kickoff
+tar czvf ./Compressed-Archives/neovim-0.9.tar.gz ./neovim-0.9
+tar czvf ./Compressed-Archives/wbg.tar.gz ./wbg
+tar czvf ./Compressed-Archives/wlr-randr.tar.gz ./wlr-randr
+tar czvf ./Compressed-Archives/yambar.tar.gz ./yambar
+tar czvf ./Compressed-Archives/FontAwesome.tar.gz ./FontAwesome
+tar czvf ./Compressed-Archives/bright.tar.gz ./bright
+#tar czvf ./Compressed-Archives/libjpeg.tar.gz ./libjpeg
