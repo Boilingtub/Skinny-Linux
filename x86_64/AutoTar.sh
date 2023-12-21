ac338ace44a1070370a47ee85fdf69a110a2304a
Boilingtub <74053750+Boilingtub@users.noreply.github.com>
2023-12-21 15:54:00 +0200

    Delete x86_64 directory

--- x86_64/AutoTar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "/home/hendrik/Skinny-Linux/Source"
-tar czvf ../x86_64/Skinny-Linux-appmenu.tar.gz       ./Skinny-Linux-appmenu
-tar czvf ../x86_64/Skinny-Linux-theme.tar.gz         ./Skinny-Linux-theme
-tar czvf ../x86_64/Suckless-Software.tar.gz          ./Suckless-Software
-tar czvf ../x86_64/Skinny-Neovim.tar.gz              ./Skinny-Neovim
-tar czvf ../x86_64/Skinny-Linux-fonts.tar.gz         ./Skinny-Linux-fonts
-tar czvf ../x86_64/Wayland-Software.tar.gz           ./Wayland-Software
