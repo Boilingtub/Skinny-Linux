924862d6642bf4392b73716a1b768f450a5f14df
Boilingtub <jhbrink59@gmail.com>
2026-01-11 17:05:54 +0200

    corrected time drift in datetime module of skstatus

--- components/skstatus/src/main.zig
+++ components/skstatus/src/main.zig
@@ -12,7 +12,7 @@ pub fn main() !void {
     while (true) {
         const BAT = Battery.query("BAT0");
         const BL = Backlight.query();
-        const DT = DateTime.query(-4);
+        const DT = DateTime.query(2);
         const WF = Wifi.query("wlp0s20f3");
         print(" ({c}):{d}%[{d}h {d}m] :{d}%  :{s}[{d}] | {d}:{d:0>2} {s} {d}/{d:0>2}/{d}\n", .{
             BAT.status,BAT.capacity,BAT.time_remaining[0],BAT.time_remaining[1],
